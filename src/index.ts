128b0db424a633bee55cce6efe8caa25cc7047ae
Alex Bechmann <alex.bechmann@outlook.com>
2018-05-11 22:23:10 +0200

    tidyup

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export function combineContainers<TComponent extends React.ComponentType<TProps>, TProps>(
   component: React.ComponentType<TProps>,
-  callbacks: Array<(containerFunctions: React.ComponentType<TProps>) => any>
+  callbacks: Array<(_: React.ComponentType<TProps>) => any>
 ): React.ComponentType<any> {
   let c = component;
   callbacks.forEach(callback => {
