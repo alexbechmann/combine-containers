4e87f60316bdf239b1eae31ad03e5cb1d9ed73d8
Alex Bechmann <alex.bechmann@outlook.com>
2018-07-08 01:22:50 +0200

    refactored function signature

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-export function combineContainers(
-  component: any,
-  callbacks: Array<(_: any) => any>
-): any {
+export const combineContainers = (callbacks: Array<(_: any) => any>) => (component: any) => {
   let c = component;
   callbacks.forEach(callback => {
     c = callback(c);
